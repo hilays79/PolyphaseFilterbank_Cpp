e717e0eb913f1054a2fb473c655d0f1a9adb68d2
Hilay Shah <Hilay.Shah@anu.edu.au>
2026-05-01 10:52:07 +1000

    Added CMake functionality for compilation on Linux and MacOS systems

---
+++ build/CMakeFiles/pfb_app.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pfb_app.dir/src/PFB.cpp.o"
+  "CMakeFiles/pfb_app.dir/src/PFB.cpp.o.d"
+  "CMakeFiles/pfb_app.dir/src/dsp_utils.cpp.o"
+  "CMakeFiles/pfb_app.dir/src/dsp_utils.cpp.o.d"
+  "CMakeFiles/pfb_app.dir/src/test_signals.cpp.o"
+  "CMakeFiles/pfb_app.dir/src/test_signals.cpp.o.d"
+  "pfb_app"
+  "pfb_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pfb_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/pfb_app.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pfb_app.
+# This may be replaced when dependencies are built.
